58dda59ae3f628a265de06fc842021fb2660ed44
Julio Fragoso <juliofragoso@gmail.com>
2021-08-26 00:40:02 -0300

    UI

--- src/pages/start.js
+++ src/pages/start.js
@@ -15,13 +15,16 @@ function Start() {
                     <p className="text-xl text-center fredoka text-black mb-5">Vamos ver se você é bom de palpite?</p>
                 </header>
                 <div className="flex flex-row items-center justify-around bg-red-700 rounded-3xl px-10 py-8 w-7/12 h-96 py-14">
-                    <div id="casual" className="flex flex-1 border-r-2 border-red-800 h-full items-center justify-center flex-col">
+                    <div id="casual" className="flex flex-1 border-r-2 border-red-800 h-full items-center justify-center flex-col px-8">
+                        <h3 className="text-3xl text-left fredoka text-white mb-8">CASUAL</h3>
                         <p id="disclaimer" className="text-md text-left fredoka text-white">Aqui é pra entrar e já se divertir com pessoas ao redor do mundo. Será que você é bom o bastante?</p>
-                        <input type="text" placeholder="Seu nick" name="chute" id="nick" className="mt-5 outline-none w-60 h-16 text-center mx-auto rounded-md text-2xl fredoka cursor-pointer shadow-md shadow-opacity-100" />
-                        <Link to="/casual" id="btCasual" className="py-3 mt-8 border-4 border-green-600 border-opacity-60 px-6 text-2xl text-green-900 rounded-xl bg-green-400 fredoka shadow-md cursor-pointer">CASUAL</Link>  
+                        <input type="text" placeholder="Seu nick" id="nick" className="mt-5 outline-none w-60 h-24 text-center mx-auto rounded-md text-2xl fredoka cursor-pointer shadow-md shadow-opacity-100" />
+                        <Link to="/casual" id="btCasual" className="py-3 mt-8 border-4 border-green-600 border-opacity-60 px-6 text-2xl text-green-900 rounded-xl bg-green-400 fredoka shadow-md cursor-pointer">JOGAR</Link>  
                     </div>
-                    <div id="lobby" className="flex flex-1 h-full w-full items-end justify-center ">
-                        <Link to="/lobby" id="btLobby"  className="py-3 border-4 border-yellow-600 border-opacity-60 ml-5 px-6 text-2xl text-yellow-900 rounded-xl bg-yellow-400 fredoka shadow-md cursor-pointer">SALA FECHADA</Link>
+                    <div id="lobby" className="flex flex-1 h-full w-full items-end justify-center items-center justify-center flex-col px-10">
+                        <h3 className="text-3xl text-left fredoka text-white mb-8">SALA FECHADA</h3>
+                        <p id="disclaimer" className="text-md text-left fredoka text-white mb-8">Crie um lugarzinho para desafiar aquele amigo que se acha sortudo. Mande o código para ele e divirtam-se!</p>
+                        <Link to="/lobby" id="btLobby"  className="py-3 border-4 border-yellow-600 border-opacity-60 ml-5 px-6 text-2xl text-yellow-900 rounded-xl bg-yellow-400 fredoka shadow-md cursor-pointer">CRIAR LOBBY</Link>
                     </div>
                 </div>
             </div>
