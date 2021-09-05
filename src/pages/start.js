40ba78c2e34fc095a0fd6fe1d899bef16446ddd3
Eric Fragoso <ericfragoso@gmail.com>
2021-09-05 00:36:07 -0300

    Agora vai navegar direto

--- src/pages/start.js
+++ src/pages/start.js
@@ -21,12 +21,12 @@ function Start() {
         
         socket.on("connected", data => {
             console.log(data);
-            history.push('/gameC');
+            
         });
 
         socket.on("startGame", data => {
             console.log(data);
-            
+            history.push('/gameC');            
         });
     }
 
