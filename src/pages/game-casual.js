c7b56d180207023c9aa9aeb42d22bd908c22e857
Julio Fragoso <juliofragoso@gmail.com>
2021-09-07 23:33:53 -0300

    att

--- src/pages/game-casual.js
+++ src/pages/game-casual.js
@@ -1,8 +1,12 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState} from "react";
 import { Adsense } from '@ctrl/react-adsense';
 
 
 function GameCasual() {
+    const [players, setPlayers] = useState({
+        p1: 'Player 1',
+        p2: 'Player 2',
+    });
     const [inicial, setInicial] = useState(0);
     const [final, setFinal] = useState(0);
     const [alvo, setAlvo] = useState(0);
@@ -56,14 +60,18 @@ function GameCasual() {
                     format=""
                 />
             </div>
-            <div id="game" className="flex flex-col text-center w-3/6 mx-auto border-4 border-green-500 bg-green-700 shadow-xl px-10 py-10 rounded-3xl">
-                <p className="text-5xl text-center text-white mb-8 fredoka">O número escondido está entre {inicial} e {final}</p>
-                <input type="text" id="chute" onChange={event => testando(event)} autoComplete="off" className="outline-none w-40 h-16 text-center mx-auto rounded-md text-2xl fredoka mb-8 cursor-pointer ring-3 ring-yellow-400" />
-                <div className="flex justify-center mt-6">
-                    <button id="btChute" onClick={() => { verificaNumero(chute) }} className="py-3 border-4 border-yellow-600 border-opacity-60 w-40 text-2xl text-yellow-900 rounded-xl bg-yellow-400 fredoka shadow-md cursor-pointer" >CHUTAR</button>
+            <div id="announcer" className="flex flex-col text-center w-3/6 mx-auto">
+                <p className="text-3xl text-center text-white mb-8 fredoka mb-10">Jogo entre {players.p1} VS {players.p2}</p>
+            
+                <div id="game" className="flex flex-col text-center w-full mx-auto border-4 border-green-500 bg-green-700 shadow-xl px-10 py-10 rounded-3xl">
+                    <p className="text-5xl text-center text-white mb-8 fredoka">O número escondido está<br></br>entre {inicial} e {final}</p>
+                    <input type="text" id="chute" onChange={event => testando(event)} autoComplete="off" className="outline-none w-40 h-16 text-center mx-auto rounded-md text-2xl fredoka mb-8 cursor-pointer ring-3 ring-yellow-400" />
+                    <div className="flex justify-center mt-6">
+                        <button id="btChute" onClick={() => { verificaNumero(chute) }} className="py-3 border-4 border-yellow-600 border-opacity-60 w-40 text-2xl text-yellow-900 rounded-xl bg-yellow-400 fredoka shadow-md cursor-pointer" >CHUTAR</button>
+                    </div>
+                    <p id="dica" className="text-4xl text-center text-white mb-4 fredoka">{alvo}</p>
+                    {text && <p id="lista" className="text-4xl text-center text-white mb-4 fredoka">{text}</p>}
                 </div>
-                <p id="dica" className="text-4xl text-center text-white mb-4 fredoka">{alvo}</p>
-                {text && <p id="lista" className="text-4xl text-center text-white mb-4 fredoka">{text}</p>}
             </div>
 
             <div id="ad2" className="w-1/6 right-0 hidden sm:block items-center">
