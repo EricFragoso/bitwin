b0ba12970193991c84bbb0e4d2fc38ed346cb427
Eric Fragoso <ericfragoso@gmail.com>
2021-10-11 00:34:00 -0300

    atualizado

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 
-const Socket=io('http://be77-179-34-117-22.ngrok.io');
+const Socket=io('http://9230-179-34-117-67.ngrok.io');
 
 
 Socket.on("connected", data => {
